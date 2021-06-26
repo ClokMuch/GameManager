484085b3b4fabd9924d48e74cd5db073712b3438
ClokMuch <tm1596113667@gmail.com>
2021-06-26 19:54:16 +0800

    initial.

---
+++ config.py
@@ -0,0 +1,12 @@
+# GameManager configs
+# ver.1
+# By Clok Much
+
+'''
+suffix of dir is '\\'
+'''
+
+class Default:
+    games_dir = "D:\\Game\\"
+    games_shortcuts_dir = "D:\\Games\\!Index\\"
+    games_list = "D:\\Game\\games_lists"
